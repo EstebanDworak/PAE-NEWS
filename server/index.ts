77fff7962e190bb208a223f65bb6806195286144
Dworak <esteban.cervantes@resser.com>
2021-04-23 23:50:42 -0500

    google auth

--- server/index.ts
+++ server/index.ts
@@ -7,8 +7,11 @@ import * as logger from "morgan";
 import * as path from "path";
 import { config } from "dotenv";
 import * as mongoose from "mongoose";
+import * as passport from 'passport'
+import {OAuth2Strategy as GoogleStrategy} from 'passport-google-oauth'
 config();
-
+// 581463894179-tejqse1s4t795unbruag0upnkc3o2br2.apps.googleusercontent.com
+// tOC95xmP-iyfFkw-zIpkeKCM
 import Router from "./home";
 
 const app = express();
@@ -31,9 +34,6 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
-// Router configuration
-const router = express.Router();
-app.use("/", Router);
 
 // Create web server
 const PORT = process.env.PORT || 3000;
@@ -55,3 +55,50 @@ server.listen(PORT, () => {
   );
   console.log("Connected to mongo DB");
 })();
+
+
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// // Router configuration
+
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+ 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
+app.use("/", Router);
